acfe400fe10b80f2f048be77e0e310133fcdf6ac
yangpeixian <15889136053@163.com>
2019-10-16 16:04:48 +0800

    最新更新10,16/16:04

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 console.log('我睡觉了'); 
 console.log('我睡醒了');
-<<<<<<< HEAD
 console.log('我去刷牙');
-<<<<<<< HEAD
 console.log('cnm分支');
 console.log('这是测试分支');
-  
-=======
 console.log('这是dev分支');
->>>>>>> dev
-=======
 console.log('我去刷牙');
->>>>>>> 57f8f5009c532457af2ff82a12c3a4d3f3006760
